76df7097e7cdbd04c058eba2225ba9b5a8838344
Anand Mohan Pathak <anandpathak8317@gmail.com>
2023-08-19 13:50:45 +0530

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -8,3 +8,36 @@ SELECT name, escape_attempts from animals WHERE weight > 10.5;
 SELECT * from animals WHERE neutered = true;
 SELECT * from animals WHERE name != 'Gabumon';
 SELECT * from animals WHERE weight >= 10.4 AND weight <= 17.3;
+
+
+BEGIN TRANSACTION;
+UPDATE animals SET species='unspecified';
+SELECT * FROM animals;
+ROLLBACK;
+SELECT * FROM animals;
+
+BEGIN TRANSACTION;
+UPDATE animals SET species='digimon' WHERE name LIKE '%mon';
+UPDATE animals SET species='pokemon' where species IS NULL;
+SELECT * FROM animals;
+COMMIT;
+
+BEGIN TRANSACTION;
+DELETE FROM animals;
+ROLLBACK;
+SELECT * FROM animals;
+
+ BEGIN TRANSACTION;
+ DELETE FROM animals WHERE date_of_birth > '2022-01-01';
+ SAVEPOINT tran1;
+ UPDATE animals SET weight_kg = weight_kg * -1;
+ ROLLBACK TO del_1;
+ UPDATE animals SET weight_kg = weight_kg * -1 WHERE weight_kg < 0;
+ COMMIT;
+
+ SELECT COUNT(*) FROM animals;
+ SELECT COUNT(*) FROM animals WHERE escape_attempts = 0;
+ SELECT AVG(weight_kg) FROM animals;
+ SELECT neutered, AVG(escape_attempts) FROM animals GROUP BY neutered;
+ SELECT species, MAX(weight_kg), MIN(weight_kg) FROM animals GROUP BY species;
+ SELECT species, AVG(escape_attempts) FROM animals WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-12-31' GROUP BY species;
