fe2157966c186ad4023c9180df748ce462416529
Kamran Asif <98281453+kamranasif506@users.noreply.github.com>
2023-08-23 23:17:21 +0500

    medical_histories table created

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -4,3 +4,12 @@ CREATE TABLE patients (
     date_of_birth date
 );
 
+CREATE TABLE medical_histories(
+	id INT GENERATED ALWAYS AS IDENTITY NOT NULL PRIMARY KEY,
+    admitted_at TIMESTAMP,
+    patient_id INT,
+    status VARCHAR(120),
+    neutered BOOLEAN,
+    weight DECIMAL,
+    FOREIGN KEY (patient_id) REFERENCES patients(id)
+);
